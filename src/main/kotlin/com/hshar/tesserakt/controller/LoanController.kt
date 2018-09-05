35ae8397ea1737d0ab2ca09fb4b88d094d906f28
Hayder Sharhan <hayder.sharhan@consensys.net>
2018-09-05 12:06:19 -0500

    TS-1: Develop MVP1.1
    - Tag and build in one.

--- src/main/kotlin/com/hshar/tesserakt/controller/LoanController.kt
+++ src/main/kotlin/com/hshar/tesserakt/controller/LoanController.kt
@@ -31,6 +31,7 @@ class LoanController {
         val loan = Gson().fromJson<JsonObject>(body)
         loan["uuid"] = UUID.randomUUID().toString()
         loan["status"] = "live"
+        // TODO: See if we should honor the createdAt that's coming from the form request.
         loan["createdAt"] = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
         loan["updatedAt"] = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
         return loanRepository.insert(Gson().fromJson<Loan>(loan))
